41e1671d2375e5425078f358768c6f2ca3a880b0
Famix12 <krkory1990@gmail.com>
2023-04-08 20:57:48 +0300

    t3

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
 });
 
 window.onscroll = function () {
-  console.log(this.scrollY);
+  console.log(this.scrollY );
   if (this.scrollY >= 700) {
     goup.classList.add("show");
   } else goup.classList.remove("show");
@@ -20,6 +20,13 @@ goup.onclick = function () {
     behavior: "smooth",
   });
 };
+
+window.onscroll = () => { 
+  if(this.scrollY >= 200 && this.scrollY <= 1350 ){
+    document.getElementById('card').classList.add('dropCard');
+  }else 
+  document.getElementById('card').classList.remove('dropCard');
+}
 
 
 skill.onclick = function () {
@@ -35,4 +42,29 @@ proj.onclick = function () {
     behavior: "smooth",
   });
 };
+
+
+// let progress = document.getElementById('progress');
+
+// // progress.style.animation ='load 1s normal forwards';
+// var str = progress.innerHTML;
+// var matches = str.match(/(\d+)/);
+ 
+// if (matches) {
+//   console.log(matches);
+
+//   var keyFrames = `
+//       keyframes spinIt {
+//       0% {
+//           width :0;
+//       }
+//       100% {
+//           width : ${1};
+//       }
+//   }`;
+
+//   progress.style.innerHTML =  keyFrames.replace(1, `${matches}%`);
+//     // document.getElementById('GFG').innerHTML
+//     //         = matches[0];
+// }
 
